6020894ac488e59f77648246667efde47538e7ac
Crew Spence <csspence123@gmail.com>
2020-04-28 07:12:06 -0500

    Wrote working solution.

--- code_wars_calculate_average.js
+++ code_wars_calculate_average.js
@@ -2,7 +2,10 @@
 Write function avg which calculates average of numbers in given list.
 */
 
-function find_average(array) {
-  // your code here
-  return 0;
+const find_average = (array) => {
+  let total = 0;
+  for(let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total/array.length;
 }
